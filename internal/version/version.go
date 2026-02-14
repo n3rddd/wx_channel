70234d80499a6dca478356f6ef5fd32b4828ce8f
“nobiyou” <ltxxgs@gmail.com>
2026-02-14 12:34:53 +0800

    chore: Bump version to 5.5.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Current is the current application version
-const Current = "5.4.1"
+const Current = "5.5.1"
 
 // Repo is the GitHub repository path
 const Repo = "nobiyou/wx_channel"
